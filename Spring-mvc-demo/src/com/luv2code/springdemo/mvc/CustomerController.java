11d8316c944910cae8fc7ad645f925366b638cd5
gyana <gyana@DESKTOP-HKBFVV4>
2020-01-19 21:59:48 -0500

    Added comments to Customer Controller

--- Spring-mvc-demo/src/com/luv2code/springdemo/mvc/CustomerController.java
+++ Spring-mvc-demo/src/com/luv2code/springdemo/mvc/CustomerController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+//Hey ...This is the controller
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
